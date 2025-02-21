a9d834223a130e47aabcc6c04221e123f6fc7aef
farizalfathin <farizlimit@gmail.com>
2025-02-21 14:56:06 +0700

    fixing theme feature

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -5,9 +5,9 @@ import { ThemeProvider } from "./theme";
 export default function ContextProvider({ children }: { children: ReactNode }) {
   return (
     <AuthenticationProvider>
-      {/* <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme"> */}
-      {children}
-      {/* </ThemeProvider> */}
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        {children}
+      </ThemeProvider>
     </AuthenticationProvider>
   );
 }
